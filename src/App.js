bf406d29215710b283730ec09d71b8c8746f41e6
Alexey Petrov <lexahood@MBP-Alexey.HomeLAN>
2023-09-29 01:04:42 +0300

    3.1 Разбор составляющих базового React-шаблона

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Book } from './Book';
 import { Preloader } from './Preloader';
@@ -6,12 +5,6 @@ import { Preloader } from './Preloader';
 function App(props) {
   return props.isLoading ? <Preloader /> :(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Саламат, ты не один такой
-        </p>
-      </header>
       <Book name='Example 1' yaer='1111' price='999'>IM CHILDREN</Book>
       <Book name='Example 2' yaer='1212' price='1999' />
       <Book name='Example 3' yaer='1313' price='2999' />
