1991733fe673107d1ec4f476feb0a2af43040313
Alexey Petrov <lexahood@MacBook-Pro-Alexey.local>
2023-09-28 19:20:31 +0300

    2.3 Преобразование приложения в JSX

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Book = (props) => {
+    return <>
+      <h2>{props.name}</h2>
+      <p>{props.yaer}</p>
+      <p>{props.price}</p>
+    </>
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +18,9 @@ function App() {
           Саламат, ты не один такой
         </p>
       </header>
+      <Book name='Example 1' yaer='1111' price='999' />
+      <Book name='Example 2' yaer='1212' price='1999' />
+      <Book name='Example 3' yaer='1313' price='2999' />
     </div>
   );
 }
