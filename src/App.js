344e850970251c9c7e2e1eace6d57ce4e250861b
Alexey Petrov <lexahood@MacBook-Pro-Alexey.local>
2023-09-29 14:19:18 +0300

    3.3 Состояние компонента и управление им

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import { Book } from './Book';
 import { Preloader } from './Preloader';
 
 function App(props) {
+  const [count, handleCount] = useState(0)
   return props.isLoading ? <Preloader /> :(
     <div className="App">
       <Book name='Example 1' yaer='1111' price='999'>IM CHILDREN</Book>
       <Book name='Example 2' yaer='1212' price='1999' />
       <Book name='Example 3' yaer='1313' price='2999' />
+      <button onClick={() => handleCount(count + 1)}>Count: {count}</button>
     </div>
   );
 }
